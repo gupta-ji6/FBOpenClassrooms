6b0ed765af8744227e3a79648d81d4db2b5e379e
Ayush Gupta <ayushgupta197@gmail.com>
2018-11-23 01:03:29 +0530

    Added product template components

---
+++ src/ProductForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ProductForm extends React.Component {
+
+  render() {
+    return (
+    );
+  }
+}
+
+export default ProductForm;
---
+++ src/ProductTableHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ProductTableHeader extends React.Component {
+
+  render() {
+    return(
+    );
+  }
+}
+
+export default ProductTableHeader;
